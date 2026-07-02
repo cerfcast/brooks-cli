ebaa2c40eed0c731a813a3b68b3ada7b72672c6f
Will Hawkins <hawkinsw@obs.cr>
2026-07-02 10:47:10 -0400

    Update Error Reporting
    Signed-off-by: Will Hawkins <hawkinsw@obs.cr>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashMap, io::Read, sync::Arc};
 
-use ansi_term::Color::Red;
+use ansi_term::{Color::Red, Style};
 #[allow(
     redundant_imports,
     unused_imports,
@@ -269,106 +269,109 @@ pub enum CliError {
 }
 pub type CliResult<T> = Result<T, CliError>;
 
-#[allow(clippy::result_large_err)]
-fn compile_and_analyze(path: clio::ClioPath) -> CliResult<()> {
-    let mut f = path.clone().open().map_err(|_| CliError::BadPath)?;
 
-    let mut to_parse: Vec<u8> = vec![];
-    f.read_to_end(&mut to_parse)
-        .map_err(|_| CliError::BadPath)?;
+fn format_compiler_error(error: CompilerError, source: &str, path: &str) -> String {
+    match error {
+        CompilerError::SyntaxError(location, msg) => {
+            let mut result =
+                Style::new().underline().paint("Error:").to_string() + &format!(" {}:\n", msg);
 
-    let source = &String::from_utf8_lossy(&to_parse);
-    let (analysis_scopes, _) = common_scopes();
+            let context_len = 3usize;
+            let source_len = source.len();
+            let semantic_source_len = if source.ends_with("\n") {
+                source_len - 1
+            } else {
+                source_len
+            };
 
-    let result = match compile(source) {
-        Ok(expr) => expr,
-        Err(e) => {
-            println!("{}", format_compiler_error(e, source, &path.to_string()));
-            return Ok(());
-        }
-    };
+            let error_start = location.start;
+            let error_end = error_start + location.extent;
 
-    let result = analysis::analyze(&result, analysis_scopes);
+            let pre_error_start =
+                std::cmp::max(0, error_start as i64 - context_len as i64) as usize;
+            let pre_error_end = location.start;
 
-    match result {
-        Ok(r) => println!("Expression Type: {}", r.tipe().to_string()),
-        Err(e) => println!("{}", format_error(e, source, &path.to_string())),
-    };
-    Ok(())
-}
+            let post_error_start = std::cmp::min(source_len, error_end);
+            let post_error_end = std::cmp::min(source_len, error_end + context_len);
 
-#[allow(clippy::result_large_err)]
-fn compile_and_interpret(path: clio::ClioPath) -> CliResult<()> {
-    let mut f = path.clone().open().map_err(|_| CliError::BadPath)?;
+            let pre_context = &source[pre_error_start..pre_error_end];
+            let erroneous = &source[error_start..error_end];
+            let post_context = &source[post_error_start..post_error_end].trim_end_matches("\n");
 
-    let mut to_parse: Vec<u8> = vec![];
-    f.read_to_end(&mut to_parse)
-        .map_err(|_| CliError::BadPath)?;
-
-    let (analysis_scopes, interp_scopes) = common_scopes();
-
-    let source = &String::from_utf8_lossy(&to_parse);
-
-    let result = match compile(source) {
-        Ok(expr) => expr,
-        Err(e) => {
-            println!("{}", format_compiler_error(e, source, &path.to_string()));
-            return Ok(());
-        }
-    };
-
-    let analyzed = analysis::analyze(&result, analysis_scopes).map_err(CliError::AnalysisError)?;
-
-    let mut interp_context = MelInterpContext::default();
-
-    interp_context = interp_context
-        .update_log(LogMsgs::new(Trace))
-        .update_scopes(interp_scopes);
-    match interpreter::interpret(&analyzed, interp_context) {
-        Ok(o) => {
-            match o.val {
-                Some(o) => println!("{}", o),
-                None => println!("Value missing"),
+            // Print the error in context.
+            result += "\t";
+            if pre_error_start != 0 {
+                result += "...";
             }
-            println!("Log:");
-            println!(
-                "{}",
-                o.log.msgs(&LogMsgFormatter {
-                    newline: true,
-                    show_level: false
-                })
-            );
+            result += pre_context;
+
+            result += &Red.underline().paint(erroneous).to_string();
+
+            result += post_context;
+            if post_error_end <= semantic_source_len {
+                result += "...";
+            }
+            result += "\n";
+            result += &format!("\tat {}:{},{}", path, error_start, error_end);
+
+            result
         }
-        Err(e) => {
-            print!("Error: {e}");
-        }
-    };
-    Ok(())
+        _ => todo!(),
+    }
 }
 
-#[allow(clippy::result_large_err)]
-fn compile_and_serialize(path: clio::ClioPath) -> CliResult<()> {
-    let mut f = path.open().map_err(|_| CliError::BadPath)?;
+fn format_analysis_error(error: MelAnalysisLocatableError, source: &str, path: &str) -> String {
+    let mut result =
+        Style::new().underline().paint("Error:").to_string() + &format!(" {}:\n", error.error);
 
-    let mut to_parse: Vec<u8> = vec![];
-    f.read_to_end(&mut to_parse)
-        .map_err(|_| CliError::BadPath)?;
-
-    let compile_result = compile(&String::from_utf8_lossy(&to_parse));
-    let ast = compile_result.expect("Compilation error");
-
-    let driver = AstVisitorDriver {};
-    let visitor = AstTextSerializer {};
-    let context = AstTextSerializerContext {
-        serialized: "".to_string(),
-        indent: 0,
+    let context_len = 3usize;
+    let source_len = source.len();
+    let semantic_source_len = if source.ends_with("\n") {
+        source_len - 1
+    } else {
+        source_len
     };
-    let result = driver
-        .visit(&ast, &visitor, context)
-        .expect("Could not serialize");
-    println!("{}", result.serialized);
-    Ok(())
+
+    let error_start = error.location.start;
+    let error_end = error_start + error.location.extent;
+
+    let pre_error_start = std::cmp::max(0, error_start as i64 - context_len as i64) as usize;
+    let pre_error_end = error.location.start;
+
+    let post_error_start = std::cmp::min(source_len, error_end);
+    let post_error_end = std::cmp::min(source_len, error_end + context_len);
+
+    let pre_context = &source[pre_error_start..pre_error_end];
+    let erroneous = &source[error_start..error_end];
+    let post_context = &source[post_error_start..post_error_end].trim_end_matches("\n");
+
+    // Print the error in context.
+    result += "\t";
+    if pre_error_start != 0 {
+        result += "...";
+    }
+    result += pre_context;
+
+    result += &Red.underline().paint(erroneous).to_string();
+
+    result += post_context;
+    if post_error_end <= semantic_source_len {
+        result += "...";
+    }
+    result += "\n";
+    result += &format!("\tat {}:{},{}", path, error_start, error_end);
+
+    result
 }
+
+fn format_error(error: MelAnalysisLocatableError, source: &str, path: &str) -> String {
+    if let MelAnalysisError::CompilerError(e) = error.error {
+        format_compiler_error(e, source, path)
+    } else {
+        format_analysis_error(error, source, path)
+    }
+}
+
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
