dcc0f967544245ecb6b320d6cf51930e3af94ec2
Will Hawkins <hawkinsw@obs.cr>
2026-07-20 06:39:40 -0400

    Add LICENSE Text To Source
    Signed-off-by: Will Hawkins <hawkinsw@obs.cr>

--- src/test.rs
+++ src/test.rs
@@ -1,3 +1,20 @@
+// brooks-cli, Copyright 2026, Will Hawkins
+//
+// This file is part of brooks-cli.
+
+// This file is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 #[cfg(test)]
 mod cli_tests {
     use insta_cmd::{assert_cmd_snapshot, get_cargo_bin};
