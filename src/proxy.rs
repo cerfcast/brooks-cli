2c1d70053658f6e96cae4b072a4fb95f0ad0a971
Will Hawkins <hawkinsw@obs.cr>
2026-07-20 04:51:07 -0400

    Finish Implementing (Early) Proxy Functionality
    Signed-off-by: Will Hawkins <hawkinsw@obs.cr>

--- src/proxy.rs
+++ src/proxy.rs
@@ -2,13 +2,15 @@ use std::str::FromStr;
 
 use actix_utils::future::ok;
 use actix_web::dev::ServiceRequest;
-use actix_web::error::ErrorInternalServerError;
+use actix_web::error::{ErrorBadGateway, ErrorInternalServerError};
 use actix_web::get;
 use actix_web::http::header::{HeaderName, HeaderValue};
 use actix_web::http::uri::{Authority, PathAndQuery};
 use actix_web::{HttpRequest, dev::PeerAddr};
 
-use brooks_lib::ps::interpret::{ProcessableRequestResponse, interpret_stage};
+use brooks_lib::ps::interpret::{
+    ProcessableRequestResponse, ProcessableRequestResponseError, interpret_stage,
+};
 use brooks_lib::ps::spec::TypedStage;
 use brooks_lib::ps::verify::PsVerificationKey;
 use futures_util::FutureExt;
@@ -69,12 +71,102 @@ where
         ) {
             return ok(req.error_response(ErrorInternalServerError(e.to_string()))).boxed_local();
         }
+
+        println!("req: {:?}", req);
         let fut = self.service.call(req);
 
-        Box::pin(async move {
-            let res = fut.await?;
-            Ok(res)
-        })
+        {
+            let crs = self.crs.clone();
+            Box::pin(async move {
+                let mut res = fut.await?;
+
+                let mut ars = ActixServiceResponse(&mut res);
+                let _ = interpret_stage(
+                    &crs,
+                    &mut ars,
+                    brooks_lib::ps::interpret::PsInterpretMode::Response,
+                );
+
+                println!("I am able to handle the response now!");
+                Ok(res)
+            })
+        }
+    }
+}
+
+#[derive(Debug)]
+struct ActixServiceResponse<'a>(&'a mut ServiceResponse);
+
+impl<'a> ProcessableRequestResponse for ActixServiceResponse<'a> {
+    fn header_value(&self) -> Option<&str> {
+        None
+    }
+
+    fn headers(&self) -> &[&str] {
+        todo!()
+    }
+
+    fn set_header_value(
+        &mut self,
+        header: &str,
+        value: &str,
+    ) -> Result<(), ProcessableRequestResponseError> {
+        self.0.headers_mut().insert(
+            HeaderName::from_str(header).expect("Cannot convert header name"),
+            HeaderValue::from_str(value).expect("Cannot convert header value"),
+        );
+        Ok(())
+    }
+
+    fn remove_header(&mut self, header: &str) -> Result<(), ProcessableRequestResponseError> {
+        self.0.headers_mut().remove(header);
+        Ok(())
+    }
+
+    fn add_header(
+        &mut self,
+        header: &str,
+        value: &str,
+    ) -> Result<(), ProcessableRequestResponseError> {
+        self.0.headers_mut().append(
+            HeaderName::from_str(header).map_err(|_| ProcessableRequestResponseError::BadValue)?,
+            HeaderValue::from_str(value).map_err(|_| ProcessableRequestResponseError::BadValue)?,
+        );
+        Ok(())
+    }
+    fn uri(&self) -> Uri {
+        println!("uri: {}", self.0.request().uri());
+        http::Uri::builder()
+            .scheme(self.0.request().uri().scheme_str().unwrap_or("https"))
+            .path_and_query(
+                self.0
+                    .request()
+                    .uri()
+                    .path_and_query()
+                    .unwrap_or(&PathAndQuery::from_static(""))
+                    .as_str(),
+            )
+            .authority(
+                self.0
+                    .request()
+                    .uri()
+                    .authority()
+                    .unwrap_or(&Authority::from_static("127.0.0.1"))
+                    .to_string(),
+            )
+            .build()
+            .expect("Could not recreate Uri")
+    }
+
+    fn set_uri(&mut self, _uri: &Uri) -> Result<(), ProcessableRequestResponseError> {
+        Err(ProcessableRequestResponseError::InvalidMode)
+    }
+
+    fn set_response(&mut self, response: &u16) -> Result<(), ProcessableRequestResponseError> {
+        let sc = actix_web::http::StatusCode::from_u16(*response)
+            .map_err(|_| ProcessableRequestResponseError::BadValue)?;
+        self.0.response_mut().head_mut().status = sc;
+        Ok(())
     }
 }
 
@@ -82,7 +174,7 @@ where
 struct ActixServiceRequest<'a>(&'a mut ServiceRequest);
 
 impl<'a> ProcessableRequestResponse for ActixServiceRequest<'a> {
-    fn header_value(&self) -> &str {
+    fn header_value(&self) -> Option<&str> {
         todo!()
     }
 
@@ -90,23 +182,35 @@ impl<'a> ProcessableRequestResponse for ActixServiceRequest<'a> {
         todo!()
     }
 
-    fn set_header_value(&mut self, header: &str, value: &str) {
+    fn set_header_value(
+        &mut self,
+        header: &str,
+        value: &str,
+    ) -> Result<(), ProcessableRequestResponseError> {
         self.0.headers_mut().insert(
             HeaderName::from_str(header).expect("Cannot convert header name"),
             HeaderValue::from_str(value).expect("Cannot convert header value"),
         );
+        Ok(())
     }
 
-    fn remove_header(&mut self, header: &str) {
+    fn remove_header(&mut self, header: &str) -> Result<(), ProcessableRequestResponseError> {
         self.0.headers_mut().remove(header);
+        Ok(())
     }
 
-    fn add_header(&mut self, header: &str, value: &str) {
+    fn add_header(
+        &mut self,
+        header: &str,
+        value: &str,
+    ) -> Result<(), ProcessableRequestResponseError> {
         self.0.headers_mut().append(
-            HeaderName::from_str(header).expect("Cannot convert header name"),
-            HeaderValue::from_str(value).expect("Cannot convert header value"),
+            HeaderName::from_str(header).map_err(|_| ProcessableRequestResponseError::BadValue)?,
+            HeaderValue::from_str(value).map_err(|_| ProcessableRequestResponseError::BadValue)?,
         );
+        Ok(())
     }
+
     fn uri(&self) -> Uri {
         println!("uri: {}", self.0.uri());
         http::Uri::builder()
@@ -129,19 +233,44 @@ impl<'a> ProcessableRequestResponse for ActixServiceRequest<'a> {
             .expect("Could not recreate Uri")
     }
 
-    fn set_uri(&mut self, _uri: &Uri) {
-        todo!()
+    fn set_uri(&mut self, _uri: &Uri) -> Result<(), ProcessableRequestResponseError> {
+        self.0.head_mut().uri = actix_web::http::Uri::from_str(&_uri.to_string())
+            .map_err(|_| ProcessableRequestResponseError::BadValue)?;
+        Ok(())
     }
 
-    fn set_response(&mut self, _response: &i64) {
-        todo!()
+    fn set_response(&mut self, _response: &u16) -> Result<(), ProcessableRequestResponseError> {
+        Err(ProcessableRequestResponseError::InvalidMode)
     }
 }
 
-#[get("/")] // <- define path parameters
+fn client_added_header(hn: &str) -> bool {
+    hn == "host" || hn == "content-length" || hn == "accept-encoding" || hn == "date"
+}
+
+#[get("/proxy/")]
 async fn index(req: HttpRequest, _peer: PeerAddr) -> actix_web::Result<String> {
-    println!("proxy_url: {:?}", req.query_string());
-    Ok("Done".to_string())
+    let query_proxy_url = req.query_string();
+    println!("proxy_url: {query_proxy_url}");
+    let proxied_url = if !query_proxy_url.is_empty() {
+        &actix_web::http::Uri::from_str(req.query_string()).map_err(ErrorBadGateway)?
+    } else {
+        req.uri()
+    };
+    let client = awc::Client::default();
+    let mut request = client.get(proxied_url);
+
+    // Use any additional headers from the original query, except for the ones
+    // that are going to be set by the client (e.g., host).
+    for header in req.headers() {
+        if !client_added_header(header.0.as_str()) {
+            request = request.insert_header_if_none(header);
+        }
+    }
+
+    let mut response = request.send().await.map_err(ErrorBadGateway)?;
+    String::from_utf8(response.body().await.map_err(ErrorBadGateway)?.to_vec())
+        .map_err(ErrorBadGateway)
 }
 
 pub async fn proxy(
@@ -151,7 +280,7 @@ pub async fn proxy(
 ) -> std::io::Result<()> {
     use actix_web::{App, HttpServer};
 
-    println!("Serving on {}:{}", ip, port);
+    println!("Proxying on {}:{}", ip, port);
     HttpServer::new(move || {
         App::new()
             .wrap(ProcessingStagesMiddleware { crs: crs.clone() })
